0ba2158e2d7779acf74fe27ee0248e64f0247597
Srivats-Srihari <ss.astrofacts@gmail.com>
2025-10-15 13:39:54 +0530

    Update MealLogger.js

--- client/src/components/MealLogger.js
+++ client/src/components/MealLogger.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SERVER_BASE } from "../config";
+import {SERVER_BASE} from "../config";
 
 export default function MealLogger({ meals, setMeals }){
   const [q, setQ] = useState('');
