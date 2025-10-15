99a85e89994add04f1f7b239b1603f0187f86dfd
Srivats-Srihari <ss.astrofacts@gmail.com>
2025-10-15 14:07:04 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-SERVER_BASE = "https://viksit-bharat-project-spinnergy.onrender.com/";
-export const SERVER_BASE;
+export const SERVER_BASE= = "https://viksit-bharat-project-spinnergy.onrender.com/";
+
 
